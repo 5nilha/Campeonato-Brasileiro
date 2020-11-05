d99a18c8d0411bf8ed25edc57ed9c7e70ffd205d
Fabio <bwtdirect@gmail.com>
2020-11-05 01:45:24 -0800

    Initial Commit

---
+++ Brasileirao/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Brasileirao
+//
+//  Created by Fabio Quintanilha on 11/5/20.
+//  Copyright © 2020 Fabio Quintanilha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
